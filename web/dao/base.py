6aec39e9773fe33e58a8bf27d7a99f98f0d65d83
Eugene Gulyaev <egulyaev@academy.ru>
2025-09-09 14:05:02 +0300

    Added baseDAO

---
+++ web/dao/base.py
@@ -0,0 +1,22 @@
+from sqlalchemy import select, insert, delete
+
+from web.database import async_session_maker
+
+
+class BaseDAO:
+    model = None
+
+    @classmethod
+    async def find_by_id(cls, model_id: int):
+        async with async_session_maker() as session:
+            query = select(cls.model).filter_by(id=model_id)
+            result = await session.execute(query)
+            return result
+        
+    
+    @classmethod
+    async def add(cls, **data):
+        async with async_session_maker() as session:
+            query = insert(cls.model).values(**data)
+            await session.execute(query)
+            await session.commit()
