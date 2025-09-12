8b222240f802f32ec4a1d5343d1ff26220dba5ab
Eugene Gulyaev <egulyaev@academy.ru>
2025-09-12 12:15:34 +0300

    added versioning

--- web/app.py
+++ web/app.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi_versioning import VersionedFastAPI, version
 from sqladmin import Admin
 
 from web.admin.views import UsersAdmin
@@ -31,7 +33,12 @@ async def lifespan(app: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(
+    title="Онлайн-школа",
+    version="0.1.0",
+    root_path="/api",
+    lifespan=lifespan,
+    )
 
 
 # SECURITY = [Depends(get_bearer_token)]
@@ -57,6 +64,37 @@ app.add_middleware(
 )
 
 
+@app.get("/test_async/{id}")
+@version(1)
+async def test_async(id: int):
+    time_to_wait = randint(3, 10)
+    print(f"Получен {id}, будет выполнена за {time_to_wait} секунд")
+    asleep(time_to_wait)
+    print(f"Задача {id} завершена" )
+    return {"msg": f"Задача {id} завершена за {time_to_wait} секунд"}
+
+
+@app.get("/test_async/{id}")
+@version(2)
+async def test_async(id: int):
+    time_to_wait = randint(3, 20)
+    print(f"Получен {id}, будет выполнена за {time_to_wait} секунд")
+    asleep(time_to_wait)
+    print(f"Задача {id} завершена" )
+    return {"msg": f"Задача {id} завершена за {time_to_wait} секунд"}
+
+
+app = VersionedFastAPI(
+    app,
+    "{major}",
+    prefix_format="/api/v{major}",
+    lifespan=lifespan,
+    # enable_latest=True,
+    )
+
+app.mount("/static", StaticFiles(directory="web/static"), "static")
+
+
 @app.get("/")
 def read_root():
     logging.info("Exectuted /")
@@ -72,13 +110,7 @@ def test_sync(id: int):
     return {"msg": f"Задача {id} завершена за {time_to_wait} секунд"}
 
 
-@app.get("/test_async/{id}")
-async def test_async(id: int):
-    time_to_wait = randint(3, 10)
-    print(f"Получен {id}, будет выполнена за {time_to_wait} секунд")
-    asleep(time_to_wait)
-    print(f"Задача {id} завершена" )
-    return {"msg": f"Задача {id} завершена за {time_to_wait} секунд"}
+
 
 
 admin = Admin(app, engine, authentication_backend=authentication_backend)
